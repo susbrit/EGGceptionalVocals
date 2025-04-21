30d665af5251c2b97846702c473ed8f74a7cfd99
Melina Castillo <mcastill@andrew.cmu.edu>
2025-04-21 00:19:24 -0400

    add test_proc_pitch(), format output printing

--- src/tests/backend_test.py
+++ src/tests/backend_test.py
@@ -1,14 +1,40 @@
 import os
 import pprint
-from .context import proc_data
+
+#from src.proc_data import TIME_STEP
+#from .context import proc_data
+#from .context import utils
+from .context import *
+def test_proc_pitch():
+    total_count = 0
+    pfs = []
+    pfs.append('src/tests/data/pitch_data/Izzy C Major Scale Mic Only.xlsx')
+    pfs.append('src/tests/data/pitch_data/Amit C Major.xlsx')
+    pfs.append('src/tests/data/pitch_data/Teresa G Major Scale.xlsx')
+    pfs.append('src/tests/data/pitch_data/Shawn A Major Scale.xlsx')
+
+    for pf in pfs:
+        print(pf)
+        wb = openpyxl.load_workbook(pf)
+        times, pitches = utils.pitch.proc_freq_data(wb, TIME_STEP)
+        total = len(pitches)
+        total_count += total
+        pprint.pp(pitches)
+        print('total: ', total)
+        print('total count: ', total_count)
 
 def test_proc_data(): 
     pf = 'src/tests/data/pitch_data/Izzy C Major Scale Mic Only.xlsx'
     cf = 'src/tests/data/cq_data/dummy_cq_1.csv'
     p_ticks, times, pitches, cqs = proc_data.proc_data(pf, cf)
+
+    print('p-axis ticks:')
     print(p_ticks)
+    print('\ntimes:')
     pprint.pp(times)
+    print('\npitches:')
     pprint.pp(pitches)
+    print('\nCQs')
     pprint.pp(cqs)
 
 def test_proc_data_sets():
@@ -20,8 +46,14 @@ def test_proc_data_sets():
     data_sets = [(pf, cf1), (pf, cf2)]
 
     p_ticks, data_sets_proc = proc_data.proc_data_sets(data_sets)
+    print('p-axis ticks:')
     print(p_ticks)
-    print(data_sets_proc)
+    for keys,values in data_sets_proc.items():
+        print('\ndata set:')
+        pprint.pp(keys)
+        print('\npitch to CQ average:')
+        pprint.pp(values)
+
 
 def test_backend():
     pitch_data = 'src/tests/data/pitch_data/'
@@ -36,6 +68,7 @@ def test_backend():
         #test_map_data(pitches, cqs)
 
 # Run tests
-#test_backend()
-test_proc_data()
+#test_proc_pitch()
+#test_proc_data()
 test_proc_data_sets()
+#test_backend()
--- src/tests/context.py
+++ src/tests/context.py
@@ -2,3 +2,6 @@ import os, sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import proc_data
+import utils
+from proc_data import TIME_STEP
+import openpyxl
